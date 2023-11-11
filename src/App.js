8a7dbb126f60031cc01925d314469c060413568a
HYHYJ <jjangdaram@naver.com>
2023-11-11 23:41:40 +0900

    API 가져오기!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
   // 변수처럼 사용
   const dataId = useRef(0);
+
+  //데이터 가져오는 함수
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        //Math.floor 정수로 바꿔줌, 0~4여서 +1 해줌
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        // id 만들기. useRef 의 .current
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  //라이프 사이클, 컴포넌트가 생성되면
+  useEffect(() => {
+    // 데이터를 가져온다.
+    getData();
+  }, []);
 
   //새로운 데이터를 추기하는 함수
   const onCreate = (author, content, emotion) => {
@@ -43,7 +70,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />{" "}
       {/* 더미 리스트 프롭스로 전달 */}
